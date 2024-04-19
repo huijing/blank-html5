afeadca48288f63e9860202b67ba1c2df04d29b4
Chen Hui Jing <1461498+huijing@users.noreply.github.com>
2024-04-19 10:26:34 +0800

    Overdue dependency bump

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,76 +1,79 @@
-const gulp         = require('gulp');
-const browserSync  = require('browser-sync');
-const sass         = require('@selfisekai/gulp-sass');
-const postcss      = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const cssnano      = require('cssnano');
-const concat       = require('gulp-concat');
-const uglify       = require('gulp-uglify');
-const babel        = require('gulp-babel');
+const gulp = require("gulp");
+const browserSync = require("browser-sync");
+const sass = require("@selfisekai/gulp-sass");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify");
+const babel = require("gulp-babel");
 
-sass.compiler = require('sass');
+sass.compiler = require("sass");
 
 const startServer = (done) => {
   browserSync.init({
     server: "./",
-    port: 2314
-  })
-  done()
-}
+    port: 2314,
+  });
+  done();
+};
 
 const browserSyncReload = (done) => {
-  browserSync.reload()
-  done()
-}
+  browserSync.reload();
+  done();
+};
 
-const compileScripts = () => { 
-  return gulp.src(['js/*.js', 'js/custom.js'])
-  .pipe(babel({
-    "presets": [ "@babel/preset-env" ]
-  }))
-  .pipe(concat('scripts.js'))
-  .pipe(gulp.dest('./'))
-  .pipe(browserSync.reload({ stream:true }))
-}
+const compileScripts = () => {
+  return gulp
+    .src(["js/*.js", "js/custom.js"], { allowEmpty: true })
+    .pipe(
+      babel({
+        presets: ["@babel/preset-env"],
+      })
+    )
+    .pipe(concat("scripts.js"))
+    .pipe(gulp.dest("./"))
+    .pipe(browserSync.reload({ stream: true }));
+};
 
 const compileStyles = () => {
-  const plugins = [
-    autoprefixer(),
-    cssnano()
-  ]
-  return gulp.src('scss/styles.scss')
-  .pipe(sass({
-    includePaths: ['scss'],
-    onError: browserSync.notify
-  }))
-  .pipe(postcss(plugins))
-  .pipe(gulp.dest('./'))
-  .pipe(browserSync.reload({ stream:true }))
-}
+  const plugins = [autoprefixer(), cssnano()];
+  return gulp
+    .src("scss/styles.scss")
+    .pipe(
+      sass({
+        includePaths: ["scss"],
+        onError: browserSync.notify,
+      })
+    )
+    .pipe(postcss(plugins))
+    .pipe(gulp.dest("./"))
+    .pipe(browserSync.reload({ stream: true }));
+};
 
 const watchMarkup = () => {
-  gulp.watch(['index.html'], browserSyncReload);
-}
+  gulp.watch(["index.html"], browserSyncReload);
+};
 
 const watchScripts = () => {
-  gulp.watch(['js/*.js'], compileScripts);
-}
+  gulp.watch(["js/*.js"], compileScripts);
+};
 
-const watchStyles = () => { 
-  gulp.watch(['scss/*.scss'], compileStyles)
-}
+const watchStyles = () => {
+  gulp.watch(["scss/*.scss"], compileStyles);
+};
 
-const compile = gulp.parallel(compileScripts, compileStyles)
-compile.description = 'compile all sources'
+const compile = gulp.parallel(compileScripts, compileStyles);
+compile.description = "compile all sources";
 
 // Not exposed to CLI
-const serve = gulp.series(compile, startServer)
-serve.description = 'serve compiled source on local server at port 3000'
+const serve = gulp.series(compile, startServer);
+serve.description = "serve compiled source on local server at port 3000";
 
-const watch = gulp.parallel(watchMarkup, watchScripts, watchStyles)
-watch.description = 'watch for changes to all source'
+const watch = gulp.parallel(watchMarkup, watchScripts, watchStyles);
+watch.description = "watch for changes to all source";
 
-const defaultTasks = gulp.parallel(serve, watch)
+const defaultTasks = gulp.parallel(serve, watch);
 
 export {
   compile,
@@ -81,6 +84,6 @@ export {
   watchMarkup,
   watchScripts,
   watchStyles,
-}
+};
 
-export default defaultTasks
+export default defaultTasks;
